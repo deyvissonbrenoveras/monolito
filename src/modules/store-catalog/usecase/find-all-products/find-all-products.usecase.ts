40e85eea140c5e988dcb6f71ede5568190cb0a75
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-07-31 20:25:52 -0300

    find all products

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,21 @@
+import UseCaseInterface from "../../../@shared/domain/usecase/use-case.interface";
+import ProductGateway from "../../gateway/product.gateway";
+
+export default class FindAllProductsUseCase implements UseCaseInterface {
+
+    constructor(private productRepository: ProductGateway) {
+
+    }
+    async execute(input: any): Promise<any> {
+        const products = await this.productRepository.findAll();
+        return {
+            products: products.map((product) => ({
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                salesPrice: product.salesPrice
+            }))
+        }
+    }
+
+}
